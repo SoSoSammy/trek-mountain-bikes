8549c9322f878ef41ee14ea10f65517c6b214438
SoSoSammy <86075091+SoSoSammy@users.noreply.github.com>
2023-04-13 16:38:50 -0600

    Update collapsable mobile navigation and searchbar

--- js/navbar.js
+++ js/navbar.js
@@ -1,16 +1,25 @@
-// Create a variable for the collapsable navigation menu
+// // Create a variable for the collapsable navigation menu
 let mainNav = document.getElementById('js-menu');
-// Create a variable for the hamburger menu
+// Create a nariable for the collapsable searchbar
+let searchBar = document.getElementById('js-searchbar');
+// // Create a variable for the hamburger menu
 let navBarToggle = document.getElementById('js-navbar-toggle');
-// Create a variable for the searchbar icon
+// Create a variable for the X button in the navigation menu
+let navBarToggleX = document.getElementById('js-navbar-toggle-close');
+// // Create a variable for the searchbar icon
 let searchBarToggle = document.getElementById('js-searchbar-toggle');
 
-// When the hamburger menu is clicked on mobile, open up the navigation menu
+// // When the hamburger menu is clicked on mobile, open up the navigation menu
 navBarToggle.addEventListener('click', function () {
     mainNav.classList.toggle('nav-active');
 });
 
-// When the searchbar icon is clicked on mobile, open up the navigation menu
+// When the X button in the navigation menu is clicked on mobile, close the menu
+navBarToggleX.addEventListener('click', function () {
+    mainNav.classList.toggle('nav-active');
+});
+
+// // When the searchbar icon is clicked on mobile, show the searchbar
 searchBarToggle.addEventListener('click', function () {
-    mainNav.classList.add('nav-active');
+    searchBar.classList.toggle('searchbar-active');
 });
